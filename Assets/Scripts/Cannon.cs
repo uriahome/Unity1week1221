25d42a6018fb280980bfbbcf69fa7bb657b8cf53
uria <uria1106@gmail.com>
2020-12-23 23:29:53 +0900

    チャージと発射を切り替えるのと チャージ時間で溜めた球を発射するような関数の下を実装

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,19 +4,62 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    public GameObject ShotObject;//発射するオブジェクト
+    public float span;//弾がたまるまでの間隔
+    public float delta;//溜めている時間
+    public int MaxShotCount;//溜められる弾の最大の数
+    public int NowShotCount;//現在溜めている弾の数
+    public bool isAttack;//trueなら攻撃中、falseならチャージ中
     // Start is called before the first frame update
     void Start()
     {
-        
+        delta = 0;
+        NowShotCount = 0;
+        isAttack = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isAttack){
+            if(NowShotCount>=1){
+                StartCoroutine("Attack");
+                NowShotCount--;
+            }
+        }else{
+            if(span < delta){
+                Debug.Log("チャージ");
+                NowShotCount++;
+                delta = 0;
+                if(NowShotCount>=MaxShotCount){
+                    NowShotCount = MaxShotCount;
+                }
+            }else{
+                this.delta += Time.deltaTime;
+            }
+        }
     }
 
     public void OnClickAction(){//クリックされたときの行動
     Debug.Log("クリックされました！");
+    isAttack = !isAttack;
     }
+    public IEnumerator Attack(){
+        int Count = 0;
+        float interval = 0.1f;
+        while (true)
+        {
+            Count++;
+            if (Count > 10)
+            {
+                GameObject Fire = Instantiate(ShotObject) as GameObject;//弾の生成
+                Fire.transform.position = new Vector3(this.transform.position.x, this.transform.position.x, this.transform.position.z);//自分の場所に出す
+                Debug.Log("oaaa");
+                yield break;
+            }
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+
 }
